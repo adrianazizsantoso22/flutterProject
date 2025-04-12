8fd6e0ac48ba9047000d0ffbeb608eb21665abe2
Adrian Aziz Santoso <adrian22pass@gmail.com>
2025-04-12 21:52:29 +0700

    Update main.dart

--- src/main.dart
+++ src/main.dart
@@ -1,67 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:objectbox/objectbox.dart';
+import 'objectbox.g.dart'; // file yang dihasilkan
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final directory = await getApplicationDocumentsDirectory();
-  Hive.init(directory.path);
-
-  // Membuka box Hive untuk menyimpan data
-  var box = await Hive.openBox('myBox');
-
-  runApp(MyApp(box: box));
+  final store = await openStore(); // Membuka store ObjectBox
+  runApp(MyApp(store: store));
 }
 
 class MyApp extends StatelessWidget {
-  final Box box;
+  final Store store;
 
-  MyApp({required this.box});
+  MyApp({required this.store});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
-      home: HomeScreen(box: box),
+      title: 'My ObjectBox App',
+      home: HomeScreen(store: store),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  final Box box;
+class HomeScreen extends StatelessWidget {
+  final Store store;
 
-  HomeScreen({required this.box});
+  void addUser(String name) {
+    final user = UserModel(name: name);
+    store.box<UserModel>().put(user); // Menyimpan data
+  }
+
+  List<UserModel> getUsers() {
+    return store.box<UserModel>().getAll(); // Mengambil semua data
+  }
 
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('ObjectBox Example')),
+      body: Center(child: Text('Hello, ObjectBox!')),
+    );
+  }
 }
-
-class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _controller = TextEditingController();
-  String _greeting = 'Tidak ada pesan';
-
-  @override
-  void initState() {
-    super.initState();
-    _loadGreeting();
-  }
-
-  // Memuat pesan dari Hive
-  void _loadGreeting() {
-    try {
-      final greeting = widget.box.get('greeting', defaultValue: 'Tidak ada pesan');
-      setState(() {
-        _greeting = greeting;
-      });
-    } catch (e) {
-      print('Error loading greeting: $e');
-    }
-  }
-
-  // Menyimpan pesan ke Hive
-  void _saveGreeting() {
-    try {
-      widget.box.put('greeting', _controller.text);
-      _loadGreeting(); // Memuat ulang pesan setelah menyimpan
-      ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Pesan disimpan
